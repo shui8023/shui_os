c91212ca78eb25476b5a7bdaf3a337919004600a
shui8023 <dubingyangxiyou@gmail.com>
2014-07-09 19:07:07 +0800

    写了一个输出16进制的，发现与汇编中的地址不对，重新写

---
+++ src/test5/init/entry.c
@@ -0,0 +1,45 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  entry.c
+ *
+ *    Description:  shui 内核的入口函数
+ *
+ *        Version:  1.0
+ *        Created:  2014年04月20日 13时31分03秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ * =====================================================================================
+ */
+
+#include "screen_operate.h"
+#include "string.h"
+#include "debug.h"
+#include "panic.h"
+#include "elf.h"
+#include "idt.h"
+#include "gdt.h"
+#include "timer.h"
+
+
+int entry()
+{
+	init_debug();
+	init_gdt();
+	init_idt();
+
+	char *string = "hello word!\n";
+      
+	screen_clear();
+	printk("%s\n", string);
+  
+	panic("test");
+//	init_timer(200);
+	
+	//sti是恢复中断。cli是关闭中断。
+//	asm volatile ("sti");
+//	asm volatile ("int $0x1");
+	return 0;
+}
+
